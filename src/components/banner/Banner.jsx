d1dde38a8414333c40e7eae49ae5ea975f0dfc2c
pacarrenoe <carrenopatricia022@gmail.com>
2024-04-29 13:43:22 -0400

    Se agregan fuentes y se crea banner

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./banner.css"; // Importa el archivo de estilos
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="overlay"></div>
+      <div className="banner-content">
+        <h1 className="banner-tittle">
+          Descubre el mundo de la crianza Montessori.
+        </h1>
+        <p>
+          ¡Explora nuestra colección de camas Montessori y da con ellos un paso
+          hacia la independencia y la exploración!
+        </p>
+        <button className="cta-button">Cotiza con nosotros</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/homeout/screens/HomeoutScreen.jsx
+++ src/pages/homeout/screens/HomeoutScreen.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "../styles/homeout.css";
+import Banner from "../../../components/banner/Banner";
 
 const HomeoutScreen = () => {
-  return <div className="main-container">HomeoutScreen</div>;
+  return (
+    <div className="main-container">
+      <Banner></Banner>
+      HomeoutScreen
+    </div>
+  );
 };
 
 export default HomeoutScreen;
